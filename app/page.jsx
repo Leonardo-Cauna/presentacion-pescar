1fe3274bacc68ab547ca555537a232d47e066c71
Leonardo Cauna <leonardo.cauna@gmail.com>
2023-09-08 00:28:27 -0300

    Inicializado git y añadido carrusel

---
+++ app/page.jsx
@@ -0,0 +1,47 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <div className="h-72 carousel rounded-box carousel-vertical">
+      <div className="carousel-item">
+          <img className="h-80 w-1/2" src="https://upload.wikimedia.org/wikipedia/commons/b/b5/800x600_Wallpaper_Blue_Sky.png" alt="Burger" />
+          <div className="divider divider-horizontal">OR</div>
+          <img className="h-80 w-1/2" src="https://upload.wikimedia.org/wikipedia/commons/b/b5/800x600_Wallpaper_Blue_Sky.png" alt="Burger" />
+        </div> 
+        <div className="carousel-item">
+          <img className="h-80 w-1/2" src="https://upload.wikimedia.org/wikipedia/commons/b/b5/800x600_Wallpaper_Blue_Sky.png" alt="Burger" />
+          <div className="divider divider-horizontal">OR</div>
+          <img className="h-80 w-1/2" src="https://upload.wikimedia.org/wikipedia/commons/b/b5/800x600_Wallpaper_Blue_Sky.png" alt="Burger" />
+        </div>
+        <div className="carousel-item">
+          <img className="h-80 w-1/2" src="https://upload.wikimedia.org/wikipedia/commons/b/b5/800x600_Wallpaper_Blue_Sky.png" alt="Burger" />
+          <div className="divider divider-horizontal">OR</div>
+          <img className="h-80 w-1/2" src="https://upload.wikimedia.org/wikipedia/commons/b/b5/800x600_Wallpaper_Blue_Sky.png" alt="Burger" />
+        </div>
+        <div className="carousel-item">
+          <img className="h-80 w-1/2" src="https://upload.wikimedia.org/wikipedia/commons/b/b5/800x600_Wallpaper_Blue_Sky.png" alt="Burger" />
+          <div className="divider divider-horizontal">OR</div>
+          <img className="h-80 w-1/2" src="https://upload.wikimedia.org/wikipedia/commons/b/b5/800x600_Wallpaper_Blue_Sky.png" alt="Burger" />
+        </div>
+        <div className="carousel-item">
+          <img className="h-80 w-1/2" src="https://upload.wikimedia.org/wikipedia/commons/b/b5/800x600_Wallpaper_Blue_Sky.png" alt="Burger" />
+          <div className="divider divider-horizontal">OR</div>
+          <img className="h-80 w-1/2" src="https://upload.wikimedia.org/wikipedia/commons/b/b5/800x600_Wallpaper_Blue_Sky.png" alt="Burger" />
+        </div>
+        <div className="carousel-item">
+          <img className="h-80 w-1/2" src="https://upload.wikimedia.org/wikipedia/commons/b/b5/800x600_Wallpaper_Blue_Sky.png" alt="Burger" />
+          <div className="divider divider-horizontal">OR</div>
+          <img className="h-80 w-1/2" src="https://upload.wikimedia.org/wikipedia/commons/b/b5/800x600_Wallpaper_Blue_Sky.png" alt="Burger" />
+        </div>
+        <div className="carousel-item">
+          <img className="h-80 w-1/2" src="https://upload.wikimedia.org/wikipedia/commons/b/b5/800x600_Wallpaper_Blue_Sky.png" alt="Burger" />
+          <div className="divider divider-horizontal">OR</div>
+          <img className="h-80 w-1/2" src="https://upload.wikimedia.org/wikipedia/commons/b/b5/800x600_Wallpaper_Blue_Sky.png" alt="Burger" />
+        </div>
+      </div>
+
+      {/* <img src="https://upload.wikimedia.org/wikipedia/commons/b/b5/800x600_Wallpaper_Blue_Sky.png"></img> */}
+    </div>
+  )
+}
